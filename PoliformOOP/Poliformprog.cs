7bdb29654a936516e9e08611e4f93096a889b419
foldesipeter <foldesip24@gmail.com>
2025-01-15 08:50:43 +0100

    asd

--- PoliformOOP/Poliformprog.cs
+++ PoliformOOP/Poliformprog.cs
@@ -12,4 +12,56 @@ namespace PoliformOOP
         {
         }
     }
+    class Ember
+    {
+        public string Nev { set; get; }
+
+        private string bemut = "Üdvözlöm, a nevem";
+        public Ember(string nev)
+        {
+            Nev = nev;
+        }
+        public string beszel()
+        {
+            return (bemut + Nev);
+        }
+
+    }
+
+    class Ferfi : Ember
+    {
+        public int Kor { set; get; }
+        public Ferfi(string nev, int kor) : base(nev)
+        {
+            Kor = kor;
+        }
+    }
+    class Woman : Ember
+    {
+        public int Szemszín { set; get; }
+        public Woman(string nev, int szemszin) : base(nev)
+        {
+            Szemszín = szemszin;
+        }
+    }
+    class Haz
+    {
+        private List<Ember> lakok = new List<Ember>();
+        public void hazajon(Ember obj)
+        {
+            this.lakok.Add(obj);
+
+        }
+        public void csenget()
+        {
+            int i;
+            Random rd = new Random();
+            if (lakok.Count > 0)
+            {
+                i = rd.Next(0, lakok.Count - 1);
+                Console.WriteLine(lakok[i].beszel());
+            }
+        }
+    }
 }
+
